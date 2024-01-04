87b663ea29be07a4950e2e3b048bde53981f3fa3
marwa sedky <marwa.sedky2011@gmail.com>
2024-01-04 21:34:32 +0200

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,10 +8,8 @@ if __name__ == "__main__":
         if length == 2:
             print("{:d} argument:".format(length - 1))
         else:
-            print("{:d} argument:".format(length - 1))
+            print("{:d} arguments:".format(length - 1))
         for i in range(1, length):
             print("{:d}: {}".format(i, arguments[i]))
     else:
         print("{} arguments.".format(length - 1))
-
-
