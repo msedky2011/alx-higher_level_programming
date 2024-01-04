9c108ce91ef6e661977813807980b8918d88b4eb
marwa sedky <marwa.sedky2011@gmail.com>
2024-01-04 19:26:59 +0200

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 from add_0 import add
 
-#Here is empty code so when you import this module into anthor module
-#the global variable __name__is"__this_module_name_itself__"
-#so it print nothing
+# Here is empty code so when you import this module into another module
+# the global variable __name__ is "__this_module_name_itself__"
+# so it prints nothing
 
 if __name__ == "__main__":
-    #when you run this module directly like ./0-add.py or python3 0-add.py
-    #the global variable __name__ is "__main__"
-    #then this module is executed as ascript
+    # When you run this module directly like ./0-add.py or python3 0-add.py
+    # the global variable __name__ is "__main__"
+    # then this module is executed as a script
     a = 1
     b = 2
     result = add(a, b)
